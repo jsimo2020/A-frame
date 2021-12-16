fdabe89766d28fb2ae37ca47650946cd7eb0c5c2
jsimo2020 <90764464+jsimo2020@users.noreply.github.com>
2021-12-16 18:50:20 +0100

    Update scene3.js

--- scene3.js
+++ scene3.js
@@ -13,7 +13,7 @@ AFRAME.registerComponent('globe', {
                                        'dur': 10000});    
         
     }
-}
+});
     
 AFRAME.registerComponent('break', {
 
@@ -26,7 +26,7 @@ AFRAME.registerComponent('break', {
     });   
         
     }
-}
+});
 
 AFRAME.registerComponent('basic-scene', {
 
